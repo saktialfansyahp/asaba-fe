fca6e3cc74bbf8191c74c7bfeb9b08b772d9cb38
AinurRahmanAlfajri <76832688+AinurRahmanAlfajri@users.noreply.github.com>
2023-11-25 13:19:18 +0700

    baseURL

--- src/main.js
+++ src/main.js
@@ -5,5 +5,6 @@ import store from "./store";
 import "./assets/css/app.css";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:8080";
+// axios.defaults.baseURL = "http://127.0.0.1:8080";
+axios.defaults.baseURL = "https://asaba-be.vercel.app";
 createApp(App).use(store).use(router).mount("#app");
